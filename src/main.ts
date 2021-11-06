f8977750b74db9cf4cbe04b41849a9872b1c4023
karthik3dinteractive <karthik@3dinteractive.se>
2021-11-06 19:23:43 +0530

    chore: Added and configured amplify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
 
 Vue.config.productionTip = false;
 
