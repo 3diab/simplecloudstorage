acbae3d115f2c1097207efd027afc5eee12ef6bb
karthik3dinteractive <karthik@3dinteractive.se>
2022-01-05 01:17:33 +0530

    refactor: add s3 trigger with layer

--- amplify/backend/function/S3Trigger430ffe51/src/index.js
+++ amplify/backend/function/S3Trigger430ffe51/src/index.js
@@ -1,8 +1,26 @@
 // eslint-disable-next-line
+const { S3Client, ListObjectsV2Command , PutObjectCommand} = require("@aws-sdk/client-s3");
 exports.handler = async function (event) {
-  console.log('Received S3 event:', JSON.stringify(event, null, 2));
+  console.log("Received S3 event:", JSON.stringify(event, null, 2));
   // Get the object from the event and show its content type
-  const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
+  const Bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
   const key = event.Records[0].s3.object.key; //eslint-disable-line
-  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+  const pathArray = key.split("/");
+  const parentFolderArray = pathArray.pop();
+
+  const parentFolder = parentFolderArray.join("/");
+
+  const client = new S3Client({ region: "eu-north-1" });
+  const listObjectsInput = {
+    Bucket,
+    Delimiter: "/",
+    Prefix: parentFolder,
+  };
+  const listObjectsCommand = new ListObjectsV2Command(listObjectsInput);
+  try {
+    const listObjectsResponse = await client.send(listObjectsCommand);
+    console.log(listObjectsResponse);
+  } catch (error) {
+    console.log("error", error.message);
+  }
 };
